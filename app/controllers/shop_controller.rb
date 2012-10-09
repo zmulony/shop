9dbb2e5eb1b14439fbbdc837fbf31c0406b955eb
zmulony <sefferynka@ubuntu.(none)>
2012-10-10 00:58:02 +0200

    1st part working local

---
+++ app/controllers/shop_controller.rb
@@ -0,0 +1,5 @@
+class ShopController < ApplicationController
+	def index
+		render :text => "Welcome in my shop"
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shop::Application.config.secret_token = 'bf458ac43a076a55b9bacfbe4155fd2b070b19d8e1d9c70a88830de9b5a857d1d1379f8c1c9f9dc2bbc0b84edc1c01996ffff8c16a9ffcba7ad133f570912508'
